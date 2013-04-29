e65ce0acd537a720b70b5e88002be6c524735e4d
Hélène Verhaeghe <helene.verhaeghe27@gmail.com>
2013-04-29 17:35:08 +0200

    stay constructor & repOK

--- S2/TapeA_B.java
+++ S2/TapeA_B.java
@@ -1,9 +1,12 @@
 public class TapeA_B implements Tape {
-	private Cell readhead;
+    private Cell readhead;
 	
-	private int repOkNb=-1;
+	private int repOkNb = -1;
 	private boolean nullNextEncountered;
 	private boolean nullPreviousEncountered;
+	
+	private static final char B = ' ';
+	
 	
 	public TapeA_B(String init) throws Exception {
 	}
@@ -13,30 +16,79 @@ public class TapeA_B implements Tape {
 	}
 	
 	public boolean isSymbol(char s) throws Exception {
-		return false;
+		testChar(s);
+		return readhead.content == s;
 	}
 	
 	public void putSymbol(char s) throws Exception {
+		testChar(s); 
+		readhead.content = s; 
 	}
 
 	public void leftMove() {
+		// case of basic tape with only one box with B inside
+		if (nullNextEncountered && nullPreviousEncountered && isSymbol(B)){
+			return;
+		// case of we are leaving a end of the tape and it is B 
+		// so we have to delete the last box ton maintain the invariant
+		if (nullNextEncountered && isSymbol(B))
+			readhead.previous.next = null;
+		// case of end of the tape and we have to go into the B territory
+		if (nullPreviousEncountered){
+			readhead.previous = new Cell();
+			readhead.previous.next = readhead;
+			nullNextEncountered = false;
+		}
+		// normal case
+		readhead = readhead.previous;
 	}
 
 	public void rightMove() {
+		// case of basic tape with only one box with B inside
+		if (nullPreviousEncountered && nullNextEncountered && isSymbol(B)){
+			return;
+		// case of we are leaving a end of the tape and it is B 
+		// so we have to delete the last box ton maintain the invariant
+		if (nullPreviousEncountered && isSymbol(B))
+			readhead.next.previous = null;
+		// case of end of the tape and we have to go into the B territory
+		if (nullNextEncountered){
+			readhead.next = new Cell();
+			readhead.next.previous = readhead;
+			nullPreviousEncountered = false;
+		}
+		// normal case
+		readhead = readhead.next;
 	}
 	
 	public String toString() {
-		return null;
+		String answer = "[" + readhead.content + "]";
+		Cell temp;
+		temp = readhead.next;
+		while (temp != null){
+			answer = answer + temp.content;
+			temp = temp.next;
+		}
+		temp = readhead.previous;
+		while (temp != null){
+			answer = temp.content + answer;
+			temp = temp.previous;
+		}
+		return answer;
 	}
 	
 	private class Cell {
-		char content;
-		Cell next=null;
-		Cell previous=null;
-		int checkedFor=-1;
+		char content = B;
+		Cell next = null;
+		Cell previous = null;
+		int checkedFor = -1;
 		
 		public Cell() {
-			content=(char) 32;
 		}
+	}
+	
+	private void testChar(char s) throws Exception {
+		if (s > 126 || s < 32)
+			throw new Exception();
 	}
 }
