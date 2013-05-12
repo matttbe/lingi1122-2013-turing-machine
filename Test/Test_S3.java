5e9008177a6ab61c4d61803065d47a2c090b4fed
Matthieu Baerts <matttbe@gmail.com>
2013-05-12 11:27:51 +0200

    Test_S3.java: with expected result

--- Test/Test_S3.java
+++ Test/Test_S3.java
@@ -14,15 +14,27 @@ public class Test_S3
 			//        0   0 => add: 1 ; substract: [ ] ; multiply: [ ] ; divide: error -> if(dividend>divisor)
 			//        10 10 => subtract: [ ]; divide: error -> if(dividend>divisor)
 			// nombre très très grand: => ils utilisent des longs (alors qu'ils ne peuvent pas ;) )
-			String cTestTapes[] = {"101 10", "101 0", "0 10", "0 0", "10 10"};
-			String cMethods[] = {"add", "substract", "multiply", "divide"};
-			for (String cTape : cTestTapes)
+			final String cTestTapes[] = {"101 10", "101 0", "0 10", "0 0", "10 10"};
+			final String cMethods[] = {"add", "substract", "multiply", "divide"};
+
+			for (int i = 0; i < cTestTapes.length; i++)
 			{
-				System.out.println ("\n" + cTape);
-				for (String cMeth : cMethods)
+				String cTape = cTestTapes[i];
+				String cTapes[] = cTape.split (" ");
+				int iTapes[] = {Integer.parseInt(cTapes[0], 2), Integer.parseInt (cTapes[1], 2)};
+				int iOp[] = {iTapes[0] + iTapes[1], iTapes[0] - iTapes[1],
+						iTapes[0] * iTapes[1], iTapes[1] != 0 ? iTapes[0] / iTapes[1] : 0,
+						iTapes[1] != 0 ? iTapes[0] % iTapes[1] : 0};
+				if (iOp[1] < 0) // substract: min 0
+					iOp[1] = 0;
+				System.out.println ("\n" + cTape + " => " + iTapes[0] + " " + iTapes[1]);
+
+				for (int j = 0; j < cMethods.length; j++)
 				{
-					if (cTape.compareTo (cTestTapes[1]) == 0 && cMeth.compareTo (cMethods[3]) == 0)
+					String cMeth = cMethods[j];
+					if (i == 1 && j == 3)
 						continue; // avoid inf. loop
+					
 					System.out.print (cMeth + ": ");
 					tape = new TapeA_B (cTape);
 					try
@@ -32,7 +44,13 @@ public class Test_S3
 					{
 						System.out.print ("Exception... ");
 					}
-					System.out.println (tape);
+					if (j == 3) // modulo
+						System.out.println (tape + " expected: "
+							+ Integer.toString (iOp[3], 2) + " "
+							+ Integer.toString (iOp[4], 2) + " => "
+							+ iOp[3] + " " + iOp[4]);
+					else
+						System.out.println (tape + " expected: " + Integer.toString (iOp[j], 2) + " => " + iOp[j]);
 				}
 			}
 		} catch (Exception e) // if error in TapeA_B
